a2a1742fde0c10bbc836c88c6d9762f42c0bf222
caf <c.a.furia@gmail.com>
2020-09-13 20:17:24 +0200

    Wget requires full filename

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -u  # terminate when accessing uninitialized variable
 set -e  # terminate with error
 
 DIR=$(dirname $(which drracket))
-wget -O "$DIR" https://raw.githubusercontent.com/bugcounting/mrracket/master/mrracket
-chmod u+x "$DIR/mrracket"
+DEST="$DIR/mrracket"
+wget -O "$DEST" https://raw.githubusercontent.com/bugcounting/mrracket/master/mrracket
+chmod u+x "$DEST"
 echo "$0: installation of mrracket in $DIR successful."
 exit 0
