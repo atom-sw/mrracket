8caefc76fa1d6b73d7406cadc5285c3d0358229b
caf <c.a.furia@gmail.com>
2020-09-14 10:36:49 +0200

    Add creation of desktop launcher.

--- install.sh
+++ install.sh
@@ -5,17 +5,35 @@ set -e  # terminate with error
 
 VERSION="7.8"
 INSTALLER="racket-$VERSION-x86_64-linux.sh"
-DESTINATION="$HOME/racket/bin"
+INSTALLDIR="$HOME/racket"
+DESTINATION="$INSTALLDIR/bin"
 
 wget "https://mirror.racket-lang.org/installers/$VERSION/$INSTALLER"
 chmod u+x "$INSTALLER"
-echo "Running DrRacket installer into $DESTINATION."
+echo "Running DrRacket installer into $INSTALLDIR."
 echo "no
 3
 " | bash "./$INSTALLER"
+
+# download logo
+wget -O "$INSTALLDIR/racket-logo.svg" https://racket-lang.org/img/racket-logo.svg
+
+# add to path
 echo 'export PATH=$PATH:'"$DESTINATION" >> "$HOME/.bashrc"
+
+# add installer
+echo "#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=$DESTINATION/drracket
+Name=DrRacket
+Comment=DrRacket
+Icon=$INSTALLDIR/racket-logo.svg
+" > "$HOME/Desktop/DrRacket.desktop"
 
 wget -O "$DESTINATION/mrracket" https://raw.githubusercontent.com/bugcounting/mrracket/master/mrracket
 chmod u+x "$DESTINATION/mrracket"
 
-echo "$0: installation of DrRacket in $DESTINATION successful. Exit this shell now."
+echo "$0: installation of DrRacket in $DESTINATION successful. Exit this terminal now."
