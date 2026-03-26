a0a683bc9e7c6b1ce3db93bd1fdc73659605e0d1
caf <c.a.furia@gmail.com>
2026-03-26 15:38:36 -0700

    Update to latest Racket version.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -u  # terminate when accessing uninitialized variable
 set -e  # terminate with error
 
-VERSION="9.0"
+VERSION="9.1"
 INSTALLER="racket-$VERSION-x86_64-linux.sh"
 INSTALLDIR="$HOME/racket"
 DESTINATION="$INSTALLDIR/bin"
